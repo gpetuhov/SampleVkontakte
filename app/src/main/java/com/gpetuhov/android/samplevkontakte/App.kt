77679e06cbe804038b3bfabe3ee5fa937e9551c3
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-26 11:55:33 +0300

    Add logout

--- app/src/main/java/com/gpetuhov/android/samplevkontakte/App.kt
+++ app/src/main/java/com/gpetuhov/android/samplevkontakte/App.kt
@@ -1,6 +1,9 @@
 package com.gpetuhov.android.samplevkontakte
 
 import android.app.Application
+import com.pawegio.kandroid.toast
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.VKTokenExpiredHandler
 
 class App : Application() {
 
@@ -8,5 +11,13 @@ class App : Application() {
         super.onCreate()
 
         // Notice that in VK SDK version 2 initialization is done automatically
+
+        // But we need to listen to token expiration
+        VK.addTokenExpiredHandler(object: VKTokenExpiredHandler {
+            override fun onTokenExpired() {
+                // Token expired, do something
+                toast("Token expired")
+            }
+        })
     }
 }
--- app/src/main/java/com/gpetuhov/android/samplevkontakte/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplevkontakte/MainActivity.kt
@@ -12,6 +12,7 @@ import com.vk.api.sdk.auth.VKScope
 import kotlinx.android.synthetic.main.activity_main.*
 
 // Notice that in VK SDK version 2 we don't have to initialize VK in Application.onCreate()
+// (but we need to listen to token expiration).
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         initName()
 
         loginButton.setOnClickListener { login() }
+        logoutButton.setOnClickListener { logout() }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -47,17 +49,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun login() {
-        // Login and request access to wall posts and photos.
-        // Result is passed into onActivityResult()
-        VK.login(this, arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+    // Login and request access to wall posts and photos.
+    // Result is passed into onActivityResult()
+    private fun login() = VK.login(this, arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+
+    private fun logout() {
+        VK.logout()
+        initName()
     }
 
     private fun initName() {
-        // Restore current token
-        val token = VKAccessToken.restore(defaultSharedPreferences)
+        // If user is logged in, show user name, otherwise clear user name
+        if (VK.isLoggedIn()) {
+            // Restore current token
+            val token = VKAccessToken.restore(defaultSharedPreferences)
 
-        // Get user ID from the token (if exists)
-        userName.text = token?.userId?.toString() ?: ""
+            // Get user ID from the token (if exists)
+            userName.text = token?.userId?.toString() ?: ""
+
+        } else {
+            userName.text = ""
+        }
     }
 }
